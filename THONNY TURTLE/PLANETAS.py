1a3f8ecd4ee562dc5d0d993359219b410007f577
m.guerra@coadvil.edu.co <m.guerra@coadvil.edu.co>
2025-03-11 11:49:49 -0500

    Add files via upload

---
+++ THONNY TURTLE/PLANETAS.py
@@ -0,0 +1,16 @@
+import turtle
+
+r=100
+turtle.circle(r)
+
+turtle.penup()
+turtle.goto(150,0)
+turtle.pendown()
+r=50
+turtle.circle(r)
+
+turtle.penup()
+turtle.goto( -150, 0)
+turtle.pendown()
+r=25
+turtle.circle(r)
